3f3e72cefc8fdee2e36c8f0053e7f4faa23bffb9
Mihail Atanassov <atanassov.mihail@gmail.com>
2015-01-22 14:22:39 +0100

    Prepare merge

--- app/src/main/java/de/uni_koeln/phil_fak/spinfo/javamobile/picman/activity/PicManActivity.java
+++ app/src/main/java/de/uni_koeln/phil_fak/spinfo/javamobile/picman/activity/PicManActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
